42e8f9eebab82084d32dfb87e8e775448a8240f8
seeluck <810201584@qq.com>
2019-11-09 20:02:45 +0800

    1

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Mod Collections"
 description = "自用。整合并小修改了一些我常用的mod，随时可能再改，最好别订阅。\nSelf-use mod collections. Updating at any time. No recommend to subscribe it."
 author = "雪绕风飞"
-version = "1.13"
+version = "1.14"
 forumthread = ""
 api_version = 10
 icon_atlas = "modicon.xml"
@@ -79,6 +79,26 @@ configuration_options =
     {
         name = "wx78drop",
         label = "WX78Drop",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "ON", data = true, hover = "ON"},
+        },
+        default = false,
+    },
+    {
+        name = "smartIceMachine",
+        label = "SmartIceMachine",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "ON", data = true, hover = "ON"},
+        },
+        default = false,
+    },
+    {
+        name = "boatHealth",
+        label = "BoatHealth",
         options =
         {
             {description = "OFF", data = false, hover = "OFF"},
--- scripts/settings.lua
+++ scripts/settings.lua
@@ -4,29 +4,37 @@ TUNING.GRASSGEKKO_MORPH_CHANCE = 0
 -- 蜘蛛吃肉
 --Make spiders wait longer before trying to eat
 GLOBAL.TUNING.SPIDER_EAT_DELAY = 10		--default is 1.5
-
 --Remove edible-ness of following items
 AddPrefabPostInit("pigskin", function(inst)
 	inst:RemoveComponent("edible")
 end)
-
 AddPrefabPostInit("manrabbit_tail", function(inst)
 	inst:RemoveComponent("edible")
 end)
-
 AddPrefabPostInit("minotaurhorn", function(inst)
 	inst:RemoveComponent("edible")
 end)
-
 AddPrefabPostInit("deerclops_eyeball", function(inst)
 	inst:RemoveComponent("edible")
 end)
 
 -- 船血上限
-TUNING.BOAT.HEALTH = 2000
+if GetModConfigData("boatHealth") then
+	TUNING.BOAT.HEALTH = 1000
+end
 
 -- 蘑菇帽孢子速度
 TUNING.MUSHROOMHAT_SPORE_TIME = 20
+
+-- 自动雪球机
+if GetModConfigData("smartIceMachine") then
+	GLOBAL.TUNING.EMERGENCY_BURNT_NUMBER = 1
+	GLOBAL.TUNING.EMERGENCY_BURNING_NUMBER = 1 -- number of fires to maintain warning level one automatically
+	GLOBAL.TUNING.EMERGENCY_WARNING_TIME = 1 -- minimum length of warning period
+	GLOBAL.TUNING.EMERGENCY_RESPONSE_TIME = 2 -- BURNT_NUMBER structures must burn within this time period to trigger flingomatic emergency response
+	GLOBAL.TUNING.EMERGENCY_SHUT_OFF_TIME = 10 -- stay on for this length of time
+	GLOBAL.TUNING.FIRESUPPRESSOR_MAX_FUEL_TIME = 480*50
+end
 
 -- 吹箭
 AddPrefabPostInit("houndstooth", function(inst)
