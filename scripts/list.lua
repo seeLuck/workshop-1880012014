1177f6ea4b263669d8c1f1cd9fa9d567a380750f
seeluck <810201584@qq.com>
2019-12-16 15:57:14 +0800

    22

--- scripts/list.lua
+++ scripts/list.lua
@@ -1,7 +1,8 @@
 Birdcage 17.12.14
-Repair Combine1.0
-MiniSign 1.1.1
+Repair Combine 1.08
+MiniSign 1.1.2
 Refillable Bucket 1.0.0
 Trashcan 1.0.2
 Epic Healthbar 1.5
 Extra Equip Slots 1.8.0
+Large Boat 1.03
