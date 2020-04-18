4e32040447d52430b9c69417414283bf283c3893
seeluck <810201584@qq.com>
2020-04-18 20:14:40 +0800

    1

--- collections/modinfo.lua
+++ collections/modinfo.lua
@@ -1,7 +1,7 @@
 name = "Mod Collections"
 description = "自用。整合并小修改了一些我常用的mod，随时可能再改，最好别订阅。\nSelf-use mod collections. Updating at any time. No recommend to subscribe it."
 author = "雪绕风飞"
-version = "1.66"
+version = "1.67"
 forumthread = ""
 api_version = 10
 icon_atlas = "modicon.xml"
@@ -76,16 +76,16 @@ configuration_options =
         },
         default = false,
     },
-    {
-        name = "largeboat",
-        label = "大船",
-        options =
-        {
-            {description = "OFF", data = false, hover = "OFF"},
-            {description = "ON", data = true, hover = "ON"},
-        },
-        default = false,
-    },
+    -- {
+    --     name = "largeboat",
+    --     label = "大船",
+    --     options =
+    --     {
+    --         {description = "OFF", data = false, hover = "OFF"},
+    --         {description = "ON", data = true, hover = "ON"},
+    --     },
+    --     default = false,
+    -- },
     {
         name = "farmer",
         label = "农场",
