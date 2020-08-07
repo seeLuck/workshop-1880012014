60a2fc875339bc368a05da2c7a2d72fd32f07bcd
seeluck <810201584@qq.com>
2020-08-07 18:10:24 +0800

    2

---
+++ collections/scripts/buildingcloser.lua
@@ -0,0 +1,30 @@
+
+for _,v in pairs(GLOBAL.AllRecipes) do
+    v.min_spacing = 1
+end
+
+AddPrefabPostInit("dug_berrybush", function(inst)
+    if inst.components.deployable then
+        inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
+    end
+end)
+AddPrefabPostInit("dug_berrybush2", function(inst)
+    if inst.components.deployable then
+        inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
+    end
+end)
+AddPrefabPostInit("dug_berrybush_juicy", function(inst)
+    if inst.components.deployable then
+        inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
+    end
+end)
+AddPrefabPostInit("rock_avocado_fruit_sprout", function(inst)
+    if inst.components.deployable then
+        inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
+    end
+end)
+AddPrefabPostInit("dug_rock_avocado_bush", function(inst)
+    if inst.components.deployable then
+        inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
+    end
+end)
