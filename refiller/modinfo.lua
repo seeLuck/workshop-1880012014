c5a471fac237ae8b4a1a037f2975cb3bc7382217
seeLuck <clkclkclk008@sina.com>
2021-01-20 20:24:54 +0800

    1

---
+++ refiller/modinfo.lua
@@ -0,0 +1,19 @@
+name = "Refillable Bucket(Fixed)"
+description =   "You are now able to refill your Bucket-o-poop! \n\n" ..
+                "You can refill it with every natural item that could also fertilize."
+author = "雪绕风飞"
+version = "1.0.1"
+
+forumthread = ""
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = true
+all_clients_require_mod = true
+client_only_mod = false
+api_version = 10
+
+icon_atlas = "preview.xml"
+icon = "preview.tex"
+
+priority = 1
